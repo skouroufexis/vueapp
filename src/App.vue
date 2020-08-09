c602bf62632eea266c2f8048873b1a7740ff07b5
skouroufexis <skouroufexis@gmail.com>
2020-08-09 18:43:09 +0300

    Update App.vue
    delete helloworld

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <Tasks />
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from   './components/Header.vue';
+import Tasks from './components/Tasks.vue';
+
+
 
 export default {
   name: 'App',
+
+
   components: {
-    HelloWorld
+    Header,Tasks
+  },
+
+  methods:{
+    
+      
   }
+  
 }
 </script>
 
