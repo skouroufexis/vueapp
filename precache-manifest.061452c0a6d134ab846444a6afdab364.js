3e91b295b2c5d689d2dc361d20f3ed052b003469
skouroufexis <skouroufexis@gmail.com>
2020-12-13 19:21:32 +0200

    Deploy

---
+++ precache-manifest.061452c0a6d134ab846444a6afdab364.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e00fcefae88e7506c29",
+    "url": "/vueapp/css/app.cc9924d6.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vueapp/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "3d94c272720fdc632e5a3b57caa0d521",
+    "url": "/vueapp/index.html"
+  },
+  {
+    "revision": "8e00fcefae88e7506c29",
+    "url": "/vueapp/js/app.363b847d.js"
+  },
+  {
+    "revision": "59133a7d534463076220",
+    "url": "/vueapp/js/chunk-vendors.5582db40.js"
+  },
+  {
+    "revision": "d8342219b0ffbed9ab8b7f5f0706533b",
+    "url": "/vueapp/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vueapp/robots.txt"
+  }
+]);
